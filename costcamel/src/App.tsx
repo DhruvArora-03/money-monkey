432f13fb2416a10754d2187bed334fa62a3b1dc4
Dhruv Arora <dhruvarora.us@gmail.com>
2024-03-24 22:28:43 -0500

    install firebase

--- costcamel/src/App.tsx
+++ costcamel/src/App.tsx
@@ -2,6 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD7ZV1Z3WWLjK9zjgnPbogZeIS9Bq6xJJI',
+  authDomain: 'costcamel-b6c02.firebaseapp.com',
+  projectId: 'costcamel-b6c02',
+  storageBucket: 'costcamel-b6c02.appspot.com',
+  messagingSenderId: '725109949838',
+  appId: '1:725109949838:web:432a3cce60d9c78fe8d0df',
+  measurementId: 'G-YCZ6FFQ0VX',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
+console.log(analytics);
+
 function App() {
   return (
     <div className="App">
