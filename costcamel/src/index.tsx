c64882570f4966b39af21089b42a8eb956abb44f
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-24 23:18:31 -0500

    move firebase init to index.tsx

--- costcamel/src/index.tsx
+++ costcamel/src/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD7ZV1Z3WWLjK9zjgnPbogZeIS9Bq6xJJI',
+  authDomain: 'costcamel-b6c02.firebaseapp.com',
+  projectId: 'costcamel-b6c02',
+  storageBucket: 'costcamel-b6c02.appspot.com',
+  messagingSenderId: '725109949838',
+  appId: '1:725109949838:web:432a3cce60d9c78fe8d0df',
+  measurementId: 'G-YCZ6FFQ0VX',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
+console.log(analytics);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
