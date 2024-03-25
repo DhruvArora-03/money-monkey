1a8556966e0b137541aa2e042bcd907c8ec677f9
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-25 13:42:49 -0500

    remove unused and add authed vs unauthed

--- costcamel/src/App.tsx
+++ costcamel/src/App.tsx
@@ -1,11 +1,9 @@
-import HomePage from '@pages/Home';
-import LoginPage from '@pages/Login';
-import { getAnalytics } from 'firebase/analytics';
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from '@pages/Home'
+import LoginPage from '@pages/Login'
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyD7ZV1Z3WWLjK9zjgnPbogZeIS9Bq6xJJI',
@@ -14,24 +12,20 @@ const firebaseConfig = {
   storageBucket: 'costcamel-b6c02.appspot.com',
   messagingSenderId: '725109949838',
   appId: '1:725109949838:web:432a3cce60d9c78fe8d0df',
-};
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export const analytics = getAnalytics(app);
-export const auth = getAuth(app);
-export const firebase = getFirestore(app);
-
-type ProtectedRouteProps = {
-  children: JSX.Element;
-};
+initializeApp(firebaseConfig)
 
 function App() {
-  const [user] = useAuthState(auth);
+  const auth = getAuth()
+  const [user] = useAuthState(auth)
 
-  const Protected = (props: ProtectedRouteProps) =>
-    user ? props.children : <Navigate replace to="/login" />;
+  const Authed = (props: { children: JSX.Element }) =>
+    user ? props.children : <Navigate replace to="/login" />
+
+  const UnAuthed = (props: { children: JSX.Element }) =>
+    user ? <Navigate replace to="/" /> : props.children
 
   return (
     <div className="App">
@@ -39,15 +33,22 @@ function App() {
         <Route
           path="/"
           element={
-            <Protected>
+            <Authed>
               <HomePage />
-            </Protected>
+            </Authed>
           }
         />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/login"
+          element={
+            <UnAuthed>
+              <LoginPage />
+            </UnAuthed>
+          }
+        />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
