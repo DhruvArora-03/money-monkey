75cba1592c7c94d48b4e6326fdf5c06db95dfc1f
Dhruv Arora <dhruvarora.us@gmail.com>
2024-05-18 17:38:42 -0500

    start adding files from old version and use @ imports

--- frontend/lib/Colors.ts
+++ frontend/lib/Colors.ts
@@ -1,7 +1,18 @@
+import { ExpenseTypes } from './Types'
+
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
 export default {
+  expenses: { 
+    Housing: '#39FF14', //neon green
+    Entertainment: '#FFFF33', // neon yellow
+    Food: '#1F51FF', // neon blue
+    Clothes: '#FF3131', // neon red
+    Groceries: '#BC13FE', // neon purple
+    Necessities: '#FF10F0', // neon pink
+    Miscellaneous: '#FFA600', // neon orange
+  } satisfies  Record<ExpenseTypes, string>,
   light: {
     text: '#000',
     background: '#fff',
@@ -17,3 +28,4 @@ export default {
     tabIconSelected: tintColorDark,
   },
 };
+
