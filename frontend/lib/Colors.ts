967f25daaadbb3e4dc34f0211ef14112573931ee
Dhruv Arora <dhruvarora.us@gmail.com>
2024-05-19 20:45:57 -0500

    change yellow to gold

--- frontend/lib/Colors.ts
+++ frontend/lib/Colors.ts
@@ -5,7 +5,7 @@ const tintColor = '#2f95dc';
 export default {
   expenses: { 
     Housing: '#39FF14', //neon green
-    Entertainment: '#FFFF33', // neon yellow
+    Entertainment: '#FFDF00', // neon yellow
     Food: '#1F51FF', // neon blue
     Clothes: '#FF3131', // neon red
     Groceries: '#BC13FE', // neon purple
