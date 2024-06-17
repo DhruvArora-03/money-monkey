b4e536c5871b331fad9f174ef86edbce36e269c3
Dhruv Arora <dhruvarora.us@gmail.com>
2024-06-17 09:59:28 -0500

    get token from login

--- frontend/app/login.tsx
+++ frontend/app/login.tsx
@@ -1,16 +1,31 @@
 import Button from "@components/Button";
 import { logIn } from "@lib/Api";
+import { useState } from "react";
 import { StyleSheet, View, SafeAreaView, TextInput, } from "react-native";
 
 export default function LoginScreen() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <SafeAreaView style={styles.page}>
             <View>
-                <TextInput></TextInput>
+                <TextInput
+                    placeholder="Username"
+                    value={username}
+                    onChangeText={setUsername}
+                />
+                <TextInput
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
             </View>
             <View style={styles.buttons}>
-                <Button title="Log In" color="blue" onPress={() => {
-                    logIn('user', 'pass')
+                <Button title="Log In" color="blue" onPress={async () => {
+                    var { token } = await logIn('johndoe', 'password') as any
+                    console.log('token: ' + token)
                 }} />
             </View>
         </SafeAreaView>
--- frontend/lib/Api.ts
+++ frontend/lib/Api.ts
@@ -1,5 +1,7 @@
 import * as Crypto from 'expo-crypto';
 
+const url = 'http://localhost:8080'
+
 function hashPassword(password: string) {
     const encoder = new TextEncoder();
     const passwordBuffer = encoder.encode(password);
@@ -17,6 +19,28 @@ function hashPassword(password: string) {
 
 export async function logIn(username: string, password: string) {
     var hashed = await hashPassword(password)
-    console.log(hashed)
 
+    return fetch(url + '/auth/login', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: username,
+            password: hashed
+        }),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('response not ok, status code: ' + response.status)
+            }
+
+            return response.json()
+        })
+        .then(json => {
+            console.log(json.token)
+            return json
+        })
 }
+
