c93a17dff1ad70850121b5acc2c6f209a51e2931
Dhruv Arora <dhruvarora.us@gmail.com>
2024-06-24 11:54:42 -0500

    update DB models

--- api/model/model.go
+++ api/model/model.go
@@ -1,7 +1,15 @@
 package model
 
 type AuthInfo struct {
-	Id       int
-	Password string
-	Salt     string
+	Id       int    `db:"id"`
+	Password string `db:"password"`
+	Salt     string `db:"salt"`
+}
+
+type PlaidConnection struct {
+	Id          int    `db:"id"`
+	UserId      int    `db:"user_id"`
+	AccessToken string `db:"access_token"`
+	ItemId      string `db:"item_id"`
+	Cursor      string `db:"cursor"`
 }
