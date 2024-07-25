29b987ae4deef29dc3fc51612aa8ef5be47b184a
Dhruv Arora <dhruvarora.us@gmail.com>
2024-07-24 23:33:40 -0500

    fix import

--- web/money-monkey/app/lib/colors.ts
+++ web/money-monkey/app/lib/colors.ts
@@ -1,23 +1,24 @@
-import { ExpenseTypes } from './Types'
+import { ExpenseTypes } from "./types";
 
-const tintColor = '#2f95dc';
+const tintColor = "#2f95dc";
 
-export default {
-  expenses: { 
-    Housing: '#00C014', // green
-    Entertainment: '#00B0FF', // lighter blue
-    Food: '#1F51FF', // neon blue
-    Clothes: '#FF3131', // neon red
-    Groceries: '#BC13FE', // neon purple
-    Necessities: '#FF10F0', // neon pink
-    Miscellaneous: '#FFA600', // neon orange
-  } satisfies  Record<ExpenseTypes, string>,
+const colors = {
+  expenses: {
+    Housing: "#00C014", // green
+    Entertainment: "#00B0FF", // lighter blue
+    Food: "#1F51FF", // neon blue
+    Clothes: "#FF3131", // neon red
+    Groceries: "#BC13FE", // neon purple
+    Necessities: "#FF10F0", // neon pink
+    Miscellaneous: "#FFA600", // neon orange
+  } satisfies Record<ExpenseTypes, string>,
   theme: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColor,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColor,
   },
 };
 
+export default colors;
