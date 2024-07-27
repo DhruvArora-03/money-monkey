6d68ab9c6183fc2f21e91933e39788c3346f5ca7
Dhruv Arora <dhruvarora.us@gmail.com>
2024-07-27 12:58:44 -0500

    remove unnecessary div

--- web/money-monkey/app/ui/NavBar.tsx
+++ web/money-monkey/app/ui/NavBar.tsx
@@ -3,9 +3,9 @@ import { SignedIn, UserButton, SignedOut, SignInButton } from "@clerk/nextjs";
 export default function NavBar() {
   return (
     <nav className="flex items-center text-white flex-wrap gap-4 w-screen bg-gray-800 p-6">
-      <div>
-        <span className="font-semibold text-xl">Money Monkey</span>
-      </div>
+      <a href="/" className="font-semibold text-xl">
+        Money Monkey
+      </a>
       <a href="/">Home</a>
       <div className="flex-grow text-right">
         <SignedIn>
