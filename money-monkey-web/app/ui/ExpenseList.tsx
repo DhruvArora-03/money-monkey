2bf972b651fb32855cb0e5df2fb69d00004f3520
Dhruv Arora <dhruvarora.us@gmail.com>
2024-08-19 02:13:36 -0500

    update formatting for long names

--- money-monkey-web/app/ui/ExpenseList.tsx
+++ money-monkey-web/app/ui/ExpenseList.tsx
@@ -11,30 +11,32 @@ export default async function ExpenseList() {
 
   var prevKey = "";
   return (
-  <table className="w-full">
-    {expenses.map((e) => {
-      const key = e.date.toLocaleDateString("en-US", {
-        month: "long",
-        year: "numeric",
-        timeZone: "UTC"
-      });
+    <table className="group w-full table-fixed">
+      <tbody>
+        {expenses.map((e) => {
+        const key = e.date.toLocaleDateString("en-US", {
+          month: "long",
+          year: "numeric",
+          timeZone: "UTC"
+        });
 
-      return (
-        <React.Fragment key={key}>
-          {key != prevKey && (
-            <tr className="w-full pt-5 [&:first-child]:pt-0 border-b-4 whitespace-nowrap text-xl">
-              {prevKey = key}
+        return (
+          <React.Fragment key={key}>
+            {key != prevKey && (
+              <tr className="border-b-4 text-xl">
+                <td className="pt-5">{prevKey = key}</td>
+              </tr>
+            )}
+            <tr key={e.id} className='border-y-2'>
+              <td className="text-nowrap text-ellipsis overflow-hidden">{e.name}</td>
+              <td>{`${e.date.getUTCMonth() + 1}/${e.date.getUTCDate()}/${e.date.getUTCFullYear()}`}</td>
+              <td>{formatMoney(e.amount_cents)}</td>
+              <td>{e.category_name}</td>
             </tr>
-          )}
-          <tr key={e.id} className='border-y-2'>
-            <td>{e.name}</td>
-            <td>{`${e.date.getUTCMonth() + 1}/${e.date.getUTCDate()}/${e.date.getUTCFullYear()}`}</td>
-            <td>{formatMoney(e.amount_cents)}</td>
-            <td>{e.category_name}</td>
-          </tr>
-        </React.Fragment>
-      );
-    })}
+          </React.Fragment>
+        );
+      })}
+      </tbody>
     </table>
   );
 }
