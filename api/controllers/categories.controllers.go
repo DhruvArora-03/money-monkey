41a1d481c574a1fcf45ca1cc5ac21e3bd396d8ce
Dhruv Arora <dhruvarora.us@gmail.com>
2024-08-18 20:31:08 -0500

    update category endpoint format

--- api/controllers/categories.controllers.go
+++ api/controllers/categories.controllers.go
@@ -13,8 +13,7 @@ func newCategoriesRouter() http.Handler {
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /categories/", getCategories)
-	router.HandleFunc("GET /categories/sums/{year}", getYearCategorySums)
-	router.HandleFunc("GET /categories/sums/{year}/{month}", getMonthCategorySums)
+	router.HandleFunc("GET /categories/sums", getCategorySums)
 
 	return utils.UserIdMiddleware(router)
 }
@@ -30,13 +29,34 @@ func getCategories(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(categories)
 }
 
-func getYearCategorySums(w http.ResponseWriter, r *http.Request) {
-	year, err := strconv.Atoi(r.PathValue("year"))
+func getCategorySums(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+
+	if !params.Has("year") {
+		http.Error(w, "year is required", http.StatusBadRequest)
+		return
+	}
+	year, err := strconv.Atoi(params.Get("year"))
 	if err != nil {
 		http.Error(w, fmt.Errorf("invalid year format %w", err).Error(), http.StatusBadRequest)
 		return
 	}
 
+	if params.Has("month") {
+		month, err := strconv.Atoi(params.Get("month"))
+		if err != nil {
+			http.Error(w, fmt.Errorf("invalid month format %w", err).Error(), http.StatusBadRequest)
+			return
+		}
+
+		getMonthCategorySums(w, r, year, month)
+		return
+	}
+
+	getYearCategorySums(w, r, year)
+}
+
+func getYearCategorySums(w http.ResponseWriter, r *http.Request, year int) {
 	categorySums, err := services.GetYearCategorySums(r.Context(), year)
 	if err != nil {
 		http.Error(w, fmt.Errorf("unable to get category sums %w", err).Error(), http.StatusInternalServerError)
@@ -47,19 +67,7 @@ func getYearCategorySums(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(categorySums)
 }
 
-func getMonthCategorySums(w http.ResponseWriter, r *http.Request) {
-	year, err := strconv.Atoi(r.PathValue("year"))
-	if err != nil {
-		http.Error(w, fmt.Errorf("invalid year format %w", err).Error(), http.StatusBadRequest)
-		return
-	}
-
-	month, err := strconv.Atoi(r.PathValue("month"))
-	if err != nil {
-		http.Error(w, fmt.Errorf("invalid month format %w", err).Error(), http.StatusBadRequest)
-		return
-	}
-
+func getMonthCategorySums(w http.ResponseWriter, r *http.Request, year, month int) {
 	categorySums, err := services.GetMonthCategorySums(r.Context(), year, month)
 	if err != nil {
 		http.Error(w, fmt.Errorf("unable to get category sums %w", err).Error(), http.StatusInternalServerError)
--- money-monkey-web/app/lib/api.ts
+++ money-monkey-web/app/lib/api.ts
@@ -29,3 +29,27 @@ export async function getExpenses(): Promise<Expense[]> {
   var expenses: any[] = await res.json();
   return expenses.map((e) => ({ ...e, date: new Date(e.date) } satisfies Expense))
 }
+
+export async function getYearCategorySums(year: number): Promise<CategorySum[]> {
+  return getCategorySums(0, year);
+}
+
+export async function getCategorySums(month: number, year: number): Promise<CategorySum[]> {
+  var userId = auth().userId
+  if (userId == null) {
+    throw new Error("could not find userId")
+  }
+
+  var url = `${process.env.API_BASE}/categories/sums?year=${year}`
+  if (month != 0) {
+    url += `&month=${month}`
+  }
+
+  const res = await fetch(url, {
+    headers: { "X-User-Id": userId }
+  });
+  if (!res.ok) {
+    throw new Error(await res.text());
+  }
+  return await res.json();
+}
