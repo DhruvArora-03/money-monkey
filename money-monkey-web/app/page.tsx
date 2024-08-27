77c5fbc1093ba1e1da2f5892770b147d3b166e4b
Dhruv Arora <dhruvarora.us@gmail.com>
2024-08-27 16:21:03 -0500

    update styles on category list

--- money-monkey-web/app/page.tsx
+++ money-monkey-web/app/page.tsx
@@ -10,9 +10,9 @@ export default async function HomePage() {
     return <div>Could not fetch category sums: {(e as any).message}</div>;
   }
 
-   return (
+  return (
     <div className="flex w-screen flex-col sm:flex-row py-6 sm:px-6 items-center sm:items-start gap-6">
-      <div className="basis-full w-64 overflow-hidden sm:basis-80 flex flex-col gap-2 p-2 border-gray-300 border-2 rounded-lg bg-neutral-50">
+      <div className="basis-full w-64 overflow-hidden sm:basis-80 flex flex-col gap-2 p-6 border-gray-300 border-r-2">
         <MainSpendDisplay sums={sums} />
         <CategoryList sums={sums} />
       </div>
--- money-monkey-web/app/ui/CategoryList.tsx
+++ money-monkey-web/app/ui/CategoryList.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { formatMoney } from "@lib/money";
 
-export default async function CategoryList({sums} : {sums: CategorySum[]}) {
+export default async function CategoryList({ sums }: { sums: CategorySum[] }) {
   return (
-  <div className="w-full flex flex-col gap-1">
-    {sums.map((sum) => (
-      <div key={sum.category_id} className={`flex justify-between border-2 bg-white rounded-md p-1 hover:bg-blue-100`} style={{borderColor: sum.color}}>
-        <p>{sum.name}</p>
-        <p>{formatMoney(sum.total_cents)}</p>
-      </div>
-    ))}
-  </div>
+    <div className="w-full flex flex-col gap-1">
+      {sums.map((sum) => (
+        <div key={sum.category_id} className="flex justify-between p-1 hover:bg-blue-100">
+          <div className="flex items-center gap-2">
+            <div className="rounded-full w-3 h-3" style={{ backgroundColor: sum.color }} />
+            <p>{sum.name}</p>
+          </div>
+          <p>{formatMoney(sum.total_cents)}</p>
+        </div>
+      ))}
+    </div>
   );
 }
