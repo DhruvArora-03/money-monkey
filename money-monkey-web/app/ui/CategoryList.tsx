5fbf4e948411fb3d1261e4604dc669d507ada87f
Dhruv Arora <dhruvarora.us@gmail.com>
2024-08-31 14:48:01 -0500

    add border around color circles

--- money-monkey-web/app/ui/CategoryList.tsx
+++ money-monkey-web/app/ui/CategoryList.tsx
@@ -7,7 +7,7 @@ export default async function CategoryList({ sums }: { sums: CategorySum[] }) {
       {sums.map((sum) => (
         <div key={sum.category_id} className="flex justify-between p-1 hover:bg-blue-100">
           <div className="flex items-center gap-2">
-            <div className="rounded-full w-3 h-3" style={{ backgroundColor: sum.color }} />
+            <div className="rounded-full w-4 h-4 border-2" style={sum.total_cents > 0 ? { backgroundColor: sum.color } : {border: "none"}} />
             <p>{sum.name}</p>
           </div>
           <p>{formatMoney(sum.total_cents)}</p>
