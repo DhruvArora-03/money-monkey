32fe73a1e5409e93ac6a75d01ffe378cf6390e18
Dhruv Arora <dhruvarora.us@gmail.com>
2024-08-31 14:49:34 -0500

    update styling for lg - sm & md still wip

--- money-monkey-web/app/page.tsx
+++ money-monkey-web/app/page.tsx
@@ -5,18 +5,19 @@ import MainSpendDisplay from "@ui/MainSpendDisplay";
 
 export default async function HomePage() {
   try {
-    var sums = await getCategorySums(new Date().getMonth() + 1, new Date().getFullYear());
+    var date = new Date();
+    var sums = await getCategorySums(date.getMonth() + 1, date.getFullYear());
   } catch (e) {
     return <div>Could not fetch category sums: {(e as any).message}</div>;
   }
 
   return (
-    <div className="flex w-screen flex-col sm:flex-row py-6 sm:px-6 items-center sm:items-start gap-6">
-      <div className="basis-full w-64 overflow-hidden sm:basis-80 flex flex-col gap-2 p-6 border-gray-300 border-r-2">
+    <div className="flex w-screen flex-row md:flex-col md:py-6 md:px-6 items-start justify-center md:items-center md:justify-start gap-4">
+      <div className="basis-full overflow-hidden sm:basis-80 flex flex-col md:flex-row gap-8 pt-6 md:p-6 border-gray-300">
         <MainSpendDisplay sums={sums} />
         <CategoryList sums={sums} />
       </div>
-      <div className="w-full px-3">
+      <div className="w-fit px-3">
         <ExpenseList />
       </div>
     </div>
