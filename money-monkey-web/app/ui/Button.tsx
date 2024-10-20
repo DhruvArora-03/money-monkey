f80334bc7a7e68d8c887f1294f9ad3deee0047ae
Dhruv Arora <dhruvarora.us@gmail.com>
2024-10-20 16:41:08 -0500

    reformat button component

--- money-monkey-web/app/ui/Button.tsx
+++ money-monkey-web/app/ui/Button.tsx
@@ -1,24 +1,25 @@
 import React, { ReactNode } from "react";
 import { IconType } from "react-icons";
 
-interface ButtonProps {
-  className?: string;
-  icon?: IconType;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  Icon?: IconType;
   iconColor?: string;
-  children?: ReactNode;
-  onClick?: () => void;
-  type?: "submit" | "reset" | "button";
 }
 
-export default function Button(props: ButtonProps) {
+export default function Button({
+  Icon,
+  iconColor,
+  className,
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
-      className={`flex items-center gap-1 border-2 rounded p-1.5 hover:bg-gray-100 active:bg-gray-200 ${props.className}`}
-      onClick={props.onClick}
-      type={props.type}
+      className={`flex items-center gap-1 border-2 rounded p-1.5 hover:bg-gray-100 active:bg-gray-200 ${className}`}
+      {...props}
     >
-      {props.icon && <props.icon color={props.iconColor} size={20} />}
-      {props.children}
+      {Icon && <Icon color={iconColor} size={20} />}
+      {children}
     </button>
   );
 }
