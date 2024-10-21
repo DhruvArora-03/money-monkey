4b9e3792fff6f0c33e21fe11a2ff703c264a6c5c
Dhruv Arora <dhruvarora.us@gmail.com>
2024-10-20 19:47:52 -0500

    extract Modal stuff into its own component

--- money-monkey-web/app/ui/NewExpenseButton.tsx
+++ money-monkey-web/app/ui/NewExpenseButton.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import Button from "./Button";
-import { MdAdd, MdClose } from "react-icons/md";
+import Button from "@ui/Button";
+import { MdAdd } from "react-icons/md";
 import React, { useCallback, useEffect, useState } from "react";
 import { Field, Form, Formik, FormikHelpers, useField } from "formik";
 import { NewExpenseSchema } from "@lib/validation";
+import PopupModal from "@ui/PopupModal";
 
 type NewExpenseModalProps = {
   className?: string;
   categories: CategorySum[];
 };
 
-export default function NewExpenseModal({
+export default function NewExpenseButton({
   categories,
   className = "",
 }: NewExpenseModalProps) {
@@ -60,6 +61,7 @@ export default function NewExpenseModal({
       <Button Icon={MdAdd} disabled={visible} onClick={() => setVisible(true)}>
         New Expense
       </Button>
+
       <Formik
         initialValues={{
           name: "",
@@ -71,45 +73,14 @@ export default function NewExpenseModal({
         onSubmit={handleSubmit}
       >
         {(props) => (
-          <div
-            className={`transition w-screen h-screen fixed left-0 top-0 flex items-center justify-center ${
-              visible ? "bg-gray-200 bg-opacity-50" : "pointer-events-none"
-            }`}
-            onClick={(e) => {
-              if (visible) {
+          <Form>
+            <PopupModal
+              title="New Expense"
+              visible={visible}
+              onClose={(e) => {
                 setVisible(false);
-                props.handleReset(e);
-              }
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Escape") {
-                setVisible(false);
-                props.handleReset(e);
-              }
-            }}
-            tabIndex={0}
-          >
-            <Form
-              className={`transition-all flex justify-center flex-col gap-1 pt-2 p-4 bg-white border-2 rounded-xl [&_label]:pr-2 ${
-                visible ? "scale-100" : "scale-0"
-              }`}
-              onClick={(e) => {
-                e.stopPropagation();
               }}
             >
-              <div className="relative text-center w-full h-10">
-                <h2 className="p-2 font-bold mb-2 underline">New Expense</h2>
-                <button
-                  className="absolute top-0 right-[-0.5rem] rounded-full hover:bg-gray-400 p-0 hover:p-2 m-2 hover:m-0 transition-all "
-                  type="button"
-                  onClick={(e) => {
-                    setVisible(false);
-                    props.handleReset(e);
-                  }}
-                >
-                  <MdClose size={24} />
-                </button>
-              </div>
               <CustomField name="name" label="Name:" placeholder="Name" />
               <CustomField name="amount" label="Amount: $" placeholder="0.00" />
               <CustomField name="date" label="Date:" type="date" />
@@ -133,8 +104,8 @@ export default function NewExpenseModal({
               <Button className="self-center" type="submit">
                 Create
               </Button>
-            </Form>
-          </div>
+            </PopupModal>
+          </Form>
         )}
       </Formik>
     </div>
---
+++ money-monkey-web/app/ui/PopupModal.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { MdClose } from "react-icons/md";
+import React, { useEffect, useRef } from "react";
+import { Console } from "console";
+
+type NewExpenseModalProps = {
+  visible: boolean;
+  title: string;
+  onClose: React.EventHandler<React.SyntheticEvent>;
+  children: React.ReactNode;
+};
+
+export default function PopupModal({
+  visible,
+  title,
+  onClose,
+  children,
+}: NewExpenseModalProps) {
+  // disable scrolling
+  useEffect(() => {
+    if (visible) {
+      document.body.style.overflow = "hidden";
+      return () => {
+        document.body.style.overflow = "unset";
+      };
+    }
+  }, [visible]);
+
+  return (
+    <div
+      className={`transition w-screen h-screen fixed left-0 top-0 flex items-center justify-center ${
+        visible ? "bg-gray-200 bg-opacity-50" : "pointer-events-none"
+      }`}
+      onClick={onClose}
+      onKeyDown={(e) => {
+        if (e.key === "Escape") {
+          onClose(e);
+        }
+      }}
+      tabIndex={0}
+    >
+      <div
+        id="modal"
+        className={`transition-all flex justify-center flex-col gap-1 pt-2 p-4 bg-white border-2 rounded-xl [&_label]:pr-2 ${
+          visible ? "scale-100" : "scale-0"
+        }`}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <div className="relative text-center w-full h-10">
+          <h2 className="px-10 py-2 font-bold mb-2 underline">{title}</h2>
+          <button
+            className="absolute top-0 right-[-0.5rem] rounded-full hover:bg-gray-400 p-0 hover:p-2 m-2 hover:m-0 transition-all "
+            type="button"
+            onClick={onClose}
+          >
+            <MdClose size={24} />
+          </button>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}
