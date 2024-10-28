00f17c09c0c74bec336fabdb4134003a05805735
Dhruv Arora <dhruvarora.us@gmail.com>
2024-10-27 22:31:14 -0500

    filter and auto format

--- money-monkey-web/app/ui/CategoryList.tsx
+++ money-monkey-web/app/ui/CategoryList.tsx
@@ -9,32 +9,31 @@ export default function CategoryList({ sums }: { sums: CategorySum[] }) {
 
   return (
     <div className="w-full flex flex-col gap-1">
-      {sums.map((sum) => {
-        var cat = categories.get(sum.category_id);
-        if (!cat) {
-          return <></>;
-        }
+      {sums
+        .filter((sum) => categories.has(sum.category_id))
+        .map((sum) => {
+          const cat = categories.get(sum.category_id)!;
 
-        return (
-          <div
-            key={sum.category_id}
-            className="flex justify-between p-1 hover:bg-blue-100"
-          >
-            <div className="flex items-center gap-2">
-              <div
-                className="rounded-full w-4 h-4 border-2"
-                style={
-                  sum.total_cents > 0
-                    ? { backgroundColor: cat.color }
-                    : { border: "none" }
-                }
-              />
-              <p>{cat.name}</p>
+          return (
+            <div
+              key={sum.category_id}
+              className="flex justify-between p-1 hover:bg-blue-100"
+            >
+              <div className="flex items-center gap-2">
+                <div
+                  className="rounded-full w-4 h-4 border-2"
+                  style={
+                    sum.total_cents > 0
+                      ? { backgroundColor: cat.color }
+                      : { border: "none" }
+                  }
+                />
+                <p>{cat.name}</p>
+              </div>
+              <p>{formatMoney(sum.total_cents)}</p>
             </div>
-            <p>{formatMoney(sum.total_cents)}</p>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
