78549b96e54d5ebf3cd0f3b78d2ae465013abeb5
Dhruv Arora <dhruvarora.us@gmail.com>
2024-10-27 22:15:16 -0500

    BasicField improvements - aria and error formatting

--- money-monkey-web/app/ui/BasicField.tsx
+++ money-monkey-web/app/ui/BasicField.tsx
@@ -1,19 +1,26 @@
-import { Field, useField } from "formik";
+import { useField } from "formik";
+import { HTMLInputTypeAttribute, InputHTMLAttributes } from "react";
 
-export default function BasicField(props: {
+interface BasicFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   label: string;
   placeholder?: string;
-  type?: string;
-}) {
+  type?: HTMLInputTypeAttribute;
+  "aria-label"?: string;
+  "aria-describedby"?: string;
+}
+
+export default function BasicField(props: BasicFieldProps) {
   const [field, meta] = useField(props.name);
   return (
     <div>
       <label className="capitalize" htmlFor={field.name}>
         {props.label}
       </label>
-      <Field id={field.name} {...field} {...props} autoComplete="off" />
-      {meta.touched && <p className="text-red-500">{meta.error}</p>}
+      <input id={field.name} {...field} {...props} autoComplete="off" />
+      {meta.touched && (
+        <p className="text-red-500 text-pretty truncate w-full">{meta.error}</p>
+      )}
     </div>
   );
 }
