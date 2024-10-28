faa24b8c67028ae1a73bcddad419a1588e63459f
Dhruv Arora <dhruvarora.us@gmail.com>
2024-10-27 22:28:55 -0500

    improve number check

--- money-monkey-web/app/lib/validation.ts
+++ money-monkey-web/app/lib/validation.ts
@@ -10,15 +10,11 @@ export const NewExpenseSchema: Yup.ObjectSchema<NewExpense> = Yup.object({
     .max(50, "Too long!"),
   amount: Yup.string()
     .required("Required!")
-    .matches(
-      /^\$((\d{1,3}(,\d{3})*|\d+))(\.\d{1,2})?$/,
-      "Invalid format!"
-    )
-    .test(
-      "is-at-least-1-cent",
-      "Amount must be at least $0.01!",
-      (value) => Number.parseFloat(value.slice(1) || "0") > 0.01
-    ),
+    .matches(/^\$((\d{1,3}(,\d{3})*|\d+))(\.\d{1,2})?$/, "Invalid format!")
+    .test("is-at-least-1-cent", "Amount must be at least $0.01!", (value) => {
+      const numericValue = value ? Number.parseFloat(value.slice(1)) : 0;
+      return !Number.isNaN(numericValue) && numericValue >= 0.01;
+    }),
   date: Yup.date().required("Required!"),
   category_id: Yup.number()
     .required("Required!")
