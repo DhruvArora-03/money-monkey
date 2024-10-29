c397c3a6cffd31f844a002611c4d37ae5177dab0
Dhruv Arora <dhruvarora.us@gmail.com>
2024-10-28 21:19:47 -0500

    remove docs

--- money-monkey-web/app/types/types.ts
+++ money-monkey-web/app/types/types.ts
@@ -1,13 +1,3 @@
-/**
- * Represents an expense.
- *
- * @interface Expense
- * @property {string} id - The unique identifier for the expense.
- * @property {string} name - The name or description of the expense.
- * @property {Date} date - The date when the expense occurred.
- * @property {string} amount - The amount of the expense as a string.
- * @property {string} category_id - The ID of the category the expense belongs to.
- */
 interface Expense {
   id: string;
   name: string;
@@ -16,15 +6,6 @@ interface Expense {
   category_id: number;
 }
 
-/**
- * Represents a new expense to be added.
- *
- * @interface NewExpense
- * @property {string} name - The name or description of the expense.
- * @property {Date} date - The date when the expense occurred.
- * @property {string} amount - The amount of the expense as a string.
- * @property {number} category_id - The ID of the category the expense belongs to.
- */
 interface NewExpense {
   name: string;
   date: Date;
@@ -32,26 +13,11 @@ interface NewExpense {
   category_id: number;
 }
 
-/**
- * Represents the total sum of expenses for a category.
- *
- * @interface CategorySum
- * @property {number} category_id - The unique identifier for the category.
- * @property {number} total_cents - The total amount in cents for the category.
- */
 interface CategorySum {
   category_id: number;
   total_cents: number;
 }
 
-/**
- * Represents an expense category.
- *
- * @interface Category
- * @property {number} id - The unique identifier for the category.
- * @property {string} name - The name of the category.
- * @property {string} color - The color associated with the category.
- */
 interface Category {
   id: number;
   name: string;
