02533f1a0bd517475d2e0b14357a4c52ab2e0b48
Dhruv Arora <dhruvarora.us@gmail.com>
2024-10-29 01:37:44 -0500

    create SelectField and fix up options

---
+++ money-monkey-web/app/ui/SelectField.tsx
@@ -0,0 +1,32 @@
+import { Field, useField } from "formik";
+import React from "react";
+import { BasicFieldProps } from "@ui/BasicField";
+
+interface SelectFieldProps extends Omit<BasicFieldProps, "type"> {
+  options: React.ReactNode;
+}
+
+export default function SelectField(props: SelectFieldProps) {
+  const [field, meta] = useField(props.name);
+  return (
+    <>
+      <label htmlFor={field.name}>{props.label}</label>
+      <Field
+        id={field.name}
+        as="select"
+        {...field}
+        {...props}
+        autoComplete="off"
+        className="px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        aria-invalid={meta.touched && meta.error ? "true" : "false"}
+      >
+        {props.options}
+      </Field>
+      {meta.touched && meta.error && (
+        <p className="text-red-500 text-sm" role="alert">
+          {meta.error}
+        </p>
+      )}
+    </>
+  );
+}
