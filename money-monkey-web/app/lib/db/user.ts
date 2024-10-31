205efff978713fbcbdf1ea34fd46eb22b49a2e06
Dhruv Arora <dhruvarora.us@gmail.com>
2024-10-30 23:20:24 -0500

    include new user comamnd

---
+++ money-monkey-web/app/lib/db/user.ts
@@ -0,0 +1,19 @@
+import { DEFAULT_CATEGORIES } from "@lib/const";
+import { db } from "@lib/db";
+import { categoryTable } from "@lib/db/schema";
+import { eq } from "drizzle-orm";
+
+export async function createUser(userId: string): Promise<void> {
+  const existing = (
+    await db
+      .select({ name: categoryTable.name })
+      .from(categoryTable)
+      .where(eq(categoryTable.user_id, userId))
+  ).map((e) => e.name);
+
+  var categories = DEFAULT_CATEGORIES.filter(
+    (cat) => !existing.includes(cat.name)
+  ).map((cat) => ({ ...cat, user_id: userId }));
+
+  await db.insert(categoryTable).values(categories);
+}
