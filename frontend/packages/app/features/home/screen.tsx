fa9126a2b31abc724479f29690568a4e8ebffade
Dhruv Arora <dhruvarora.us@gmail.com>
2024-11-13 17:25:14 -0600

    add auth stuff to home screen

--- frontend/packages/app/features/home/screen.tsx
+++ frontend/packages/app/features/home/screen.tsx
@@ -1,15 +1,31 @@
-import { H1 } from 'app/design/typography'
+import { Session } from '@supabase/supabase-js'
+import { H1, Text } from 'app/design/typography'
 import { View } from 'app/design/view'
-import Auth from 'app/features/auth/Auth'
-
+import GoogleSignInButton from 'app/features/auth/GoogleSignInButton'
+import SignOutButton from 'app/features/auth/SignOutButton'
 import CategoryDonutChart from 'app/features/dashboard/CategoryDonutChart'
+import { supabase } from 'app/features/supabase'
+import { useEffect, useState } from 'react'
 
 export function HomeScreen() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
   return (
     <View className="flex-1 p-3">
       <H1>Dashboard</H1>
       <CategoryDonutChart />
-      <Auth />
+      <GoogleSignInButton />
+      {session && session.user && <Text>{session.user.id}</Text>}
+      <SignOutButton />
     </View>
   )
 }
