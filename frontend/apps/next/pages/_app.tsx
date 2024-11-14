6b98eb82fd6acba9cc46ead9c58a9665dfb3233e
Dhruv Arora <dhruvarora.us@gmail.com>
2024-11-14 16:07:38 -0600

    change import order

--- frontend/apps/next/pages/_app.tsx
+++ frontend/apps/next/pages/_app.tsx
@@ -2,11 +2,9 @@ import 'raf/polyfill'
 import 'setimmediate'
 
 import { Provider } from 'app/provider'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
 import React from 'react'
-
-import '../global.css'
-import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
