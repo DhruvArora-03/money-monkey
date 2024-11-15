03f7816ed6aee7520ddaa6804f46760e6b0ebda8
Dhruv Arora <dhruvarora.us@gmail.com>
2024-11-15 11:17:20 -0600

    get chart data

--- money-monkey-web/app/page.tsx
+++ money-monkey-web/app/page.tsx
@@ -65,13 +65,13 @@ export default async function HomePage() {
 
   return (
     <div className="flex w-screen flex-col md:p-6 items-center justify-start">
-      <CategoryPieChart />
       <NewExpenseButton
         className="absolute right-0 p-3 md:pt-0 md:pr-6"
         userId={userId}
       />
       <div className="overflow-hidden flex flex-col md:flex-row gap-8 pt-16 md:pb-6 md:pt-0 border-gray-300 max-w-7xl mx-auto">
-        <MainSpendDisplay sums={sums} />
+        {/* <MainSpendDisplay sums={sums} /> */}
+        <CategoryPieChart sums={sums} />
         <CategoryList sums={sums} />
       </div>
       <div className="w-full md:w-2/3 overflow-hidden px-6">
