07cbcf5390078d3fdbf459a0b2ef0ab689aee1e5
Dhruv Arora <dhruvarora.us@gmail.com>
2024-11-18 01:15:43 -0600

    update schema and drizzle to use supabase

--- money-monkey-web/lib/db/schema.ts
+++ money-monkey-web/lib/db/schema.ts
@@ -2,14 +2,18 @@ import { sql } from "drizzle-orm";
 import {
   date,
   integer,
+  pgPolicy,
+  pgSchema,
   pgTableCreator,
   serial,
   text,
   timestamp,
   uniqueIndex,
+  uuid,
 } from "drizzle-orm/pg-core";
 
-const createTable = pgTableCreator((name) => `money_monkey_${name}`);
+// edit this to create a table_prefix
+const createTable = pgTableCreator((name) => `${name}`);
 
 const getCreatedAtColumn = () => timestamp("created_at").defaultNow().notNull();
 const getUpdatedAtColumn = () =>
@@ -18,41 +22,80 @@ const getUpdatedAtColumn = () =>
     .$onUpdate(() => sql`current_timestamp`)
     .notNull();
 
-export const expenseTable = createTable("expense", {
-  user_id: text("user_id").notNull(),
+const authSchema = pgSchema("auth");
 
-  id: serial("id").primaryKey(),
-  category_id: integer("category_id")
-    .references(() => categoryTable.id)
-    .notNull(),
-
-  name: text("name").notNull(),
-  amount_cents: integer("amount_cents").notNull(),
-  date: date("date", { mode: "date" }).notNull(),
-
-  created_at: getCreatedAtColumn(),
-  updated_at: getUpdatedAtColumn(),
+const usersTable = authSchema.table("users", {
+  id: uuid("id").primaryKey(),
 });
+
+export const profileTable = createTable(
+  "profile",
+  {
+    // Matches id from auth.users table in Supabase
+    id: uuid("id")
+      .primaryKey()
+      .references(() => usersTable.id, { onDelete: "cascade" }),
+  },
+  () => [
+    pgPolicy("Users can view their own profile", {
+      as: "permissive",
+      for: "select",
+      to: ["public"],
+      using: sql`(select auth.uid()) = id`,
+    }),
+  ]
+);
+
+export const expenseTable = createTable(
+  "expense",
+  {
+    id: serial("id").primaryKey(),
+    profile_id: uuid("profile_id")
+      .notNull()
+      .references(() => profileTable.id),
+    category_id: integer("category_id")
+      .references(() => categoryTable.id)
+      .notNull(),
+    name: text("name").notNull(),
+    amount_cents: integer("amount_cents").notNull(),
+    date: date("date", { mode: "date" }).notNull(),
+    created_at: getCreatedAtColumn(),
+    updated_at: getUpdatedAtColumn(),
+  },
+  () => [
+    pgPolicy("Users can do anything with their own expenses", {
+      as: "permissive",
+      for: "all",
+      to: ["public"],
+      using: sql`(select auth.uid()) = profile_id`,
+    }),
+  ]
+);
 
 export const categoryTable = createTable(
   "category",
   {
-    user_id: text("user_id").notNull(),
-
     id: serial("id").primaryKey(),
-
+    profile_id: uuid("profile_id")
+      .notNull()
+      .references(() => profileTable.id),
     name: text("name").notNull(),
     color: text("color").notNull(),
-
     created_at: getCreatedAtColumn(),
     updated_at: getUpdatedAtColumn(),
   },
-  (table) => {
-    return {
-      userCategoryIdx: uniqueIndex("user_category_idx").on(
-        table.user_id,
-        table.name
-      ),
-    };
-  }
+  (t) => [
+    uniqueIndex("user_category_idx").on(t.profile_id, t.name),
+    pgPolicy("Users can do anything with their own categories", {
+      as: "permissive",
+      for: "all",
+      to: ["public"],
+      using: sql`(select auth.uid()) = profile_id`,
+    }),
+  ]
 );
+
+export type InsertExpense = typeof expenseTable.$inferInsert;
+export type SelectExpense = typeof expenseTable.$inferSelect;
+export type InsertCategory = typeof categoryTable.$inferInsert;
+export type SelectCategory = typeof categoryTable.$inferSelect;
